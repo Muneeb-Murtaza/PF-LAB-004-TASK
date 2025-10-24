3d70a4831c69494a23fbb3879e6d487bcaed8ea8
Muneeb-Murtaza <f2025-0987@bnu.edu.pk>
2025-10-24 14:51:17 +0500

    Add basic information display program

---
+++ TASK 01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: \t Muneeb Murtaza" << endl;
+    cout << "Department: \t SCIT" << endl;
+    cout << "Quote:\t \"Knowledge is power.\"" << endl;
+    return 0;
+}
